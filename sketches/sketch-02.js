2765895f43000236f3ef2033bdcc97574c45388f
Maximilian Lombardo <maximilian.g.lombardo@gmail.com>
2022-01-04 11:43:56 -0500

    finshing sketch 2 and including variations

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -1,16 +1,9 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math')
+const random = require('canvas-sketch-util/random')
 
 const settings = {
   dimensions: [ 1080, 1080 ]
-};
-
-
-const degToRad = (degrees) => {
-  return degrees / 180 * Math.PI;
-};
-
-const randomRange = (min, max) => {
-  return Math.random() * (max - min) + min;
 };
 
 const sketch = () => {
@@ -22,30 +15,47 @@ const sketch = () => {
 
     const cx = width * 0.5;
     const cy = height * 0.5;
-    const w = width * 0.01;
+    const w = width * 0.001;
     const h = height * 0.1;
     let x, y;
 
-    const num = 12;
+    const num = 1000;
     const radius = width * 0.3;
 
     for(let i = 0; i < num; i ++) {
 
-      const slice = degToRad(360 / num);
+      const slice = math.degToRad(360 / num);
       const angle = slice * i;
 
       x = cx + radius * Math.sin(angle);
       y = cy + radius * Math.cos(angle);
 
+      //
       context.save();
       context.translate(x, y);
       context.rotate(-angle);
-      context.scale(Math.random() * 3, Math.random() * 1.5);
+      context.scale(random.range(1, 3), random.range(0.5, 1));
 
       context.beginPath();
-      context.rect(-w * 0.5, -h * 0.5, w, h);
+      //context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.rect(-w * 0.5, random.range(0, -h * 1), w, h);
       context.fill();
       context.restore();
+
+      //
+      context.save();
+      context.translate(cx, cy);
+      context.rotate(-angle);
+
+      context.lineWidth = random.range(1, 2);
+
+      context.beginPath();
+
+      context.arc(0, 0, radius * random.range(0.7, 1.3), slice * random.range(0, -5), slice * random.range(1, 5));
+      context.stroke();
+
+      context.restore();
+
 
 
     }
