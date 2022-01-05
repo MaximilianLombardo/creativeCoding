d9d036bfcf29722c696c33f905e52bc2546ce27f
Maximilian Lombardo <maximilian.g.lombardo@gmail.com>
2022-01-05 09:52:02 -0500

    more layers

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -7,7 +7,7 @@ const settings = {
   // Enable an animation loop
   animate: false,
   // Set loop duration to 3
-  duration: 1000,
+  duration: 10,
   // Use a small size for better GIF file size
   dimensions: [ 1080, 1080],
 };
@@ -27,19 +27,41 @@ const sketch = () => {
     let xTwo, yTwo;
 
     const num = 10000;
+    const radiusInner = width * 0.1
     const radius = width * 0.3;
-    const radiusTwo = width * 0.35;
+    const radiusTwo = width * 0.325;
+    const radiusThree = width * 0.35;
 
     for(let i = 0; i < num; i ++) {
 
       const slice = math.degToRad(360 / num);
       const angle = slice * i;
 
+      xInner = cx + radiusInner * Math.sin(angle);
+      yInner = cy + radiusInner * Math.cos(angle);
+
       x = cx + radius * Math.sin(angle);
       y = cy + radius * Math.cos(angle);
 
       xTwo = cx + radiusTwo * Math.sin(angle);
       yTwo = cy + radiusTwo * Math.cos(angle);
+
+      xThree = cx + radiusThree * Math.sin(angle);
+      yThree = cy + radiusThree * Math.cos(angle);
+
+      // - Inner rectangles - Iris - layer 3
+
+      context.save();
+      context.translate(xInner, yInner);
+      context.rotate(-angle);
+      context.scale(random.range(1, 10), random.range(1, 2));
+
+      context.fillStyle = 'white';//'#42b3f5';
+      context.beginPath();
+      //context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.rect(-w * 0.5, random.range(0, -h * 0.1), w, random.range(-h * 0.15, h * 0.5));
+      context.fill();
+      context.restore();
 
       // - white rectanlges - layer 1
       context.save();
@@ -49,7 +71,7 @@ const sketch = () => {
 
       context.beginPath();
       //context.rect(-w * 0.5, -h * 0.5, w, h);
-      context.rect(-w * 0.5, random.range(0, -h * 0.1), w, random.range(0, h * 0.5));
+      context.rect(-w * 0.5, random.range(0, -h * 0.1), w, random.range(h * 0.45, h * 0.5));
       context.fill();
       context.restore();
 
@@ -60,12 +82,56 @@ const sketch = () => {
       context.rotate(-angle);
       context.scale(random.range(1, 10), random.range(1, 2));
 
+      context.fillStyle = 'yellow';
+      context.beginPath();
+      //context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.rect(-w * 0.5, random.range(0, -h * 0.1), w, random.range(0, h * 0.5));
+      context.fill();
+      context.restore();
+
+      if (i % 7 == 0 ){ //not sure if this is working
+        // - blue overlay rectangles - layer 2a
+
+        context.save();
+        context.translate(xTwo, yTwo);
+        context.rotate(-angle);
+        context.scale(random.range(1, 10), random.range(1, 2));
+
+        context.fillStyle = 'blue';
+        context.beginPath();
+        //context.rect(-w * 0.5, -h * 0.5, w, h);
+        context.rect(-w * 0.01, random.range(0, -h * 0.01), w, random.range(0, h * 0.01));
+        context.fill();
+        context.restore();
+      }
+      // - blue overlay rectangles - layer 2a
+
+      context.save();
+      context.translate(xTwo, yTwo);
+      context.rotate(-angle);
+      context.scale(random.range(1, 10), random.range(1, 2));
+
+      context.fillStyle = 'blue';
+      context.beginPath();
+      //context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.rect(-w * 0.5, random.range(0, -h * 0.1), w, random.range(0, h * 0.5));
+      context.fill();
+      context.restore();
+
+      // - x rectangles - layer 3
+
+      context.save();
+      context.translate(xThree, yThree);
+      context.rotate(-angle);
+      context.scale(random.range(1, 10), random.range(1, 2));
+
       context.fillStyle = 'orange';
       context.beginPath();
       //context.rect(-w * 0.5, -h * 0.5, w, h);
       context.rect(-w * 0.5, random.range(0, -h * 0.1), w, random.range(0, h * 0.5));
       context.fill();
       context.restore();
+
 
       // - black background shapes
       context.save();
@@ -77,7 +143,8 @@ const sketch = () => {
       context.strokeStyle = 'black';
       context.beginPath();
 
-      context.arc(0, 0, radius * random.range(0.7, 1.3), slice * random.range(0, -5), slice * random.range(1, 5));
+      //context.arc(0, 0, radius * random.range(0.7, 1.3), slice * random.range(0, -1), slice * random.range(1, 5));
+      context.arc(0, 0, radius * random.range(0.5, 1), slice * random.range(0, -1), slice * random.range(1, 5));
       context.stroke();
 
       context.restore();
