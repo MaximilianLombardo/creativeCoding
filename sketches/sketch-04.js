a4b4fa03648fa9fca1f4cdfa5d0b21b9a4711938
Maximilian Lombardo <maximilian.g.lombardo@gmail.com>
2022-01-21 17:45:52 -0500

    update sketch 4: animating and adding noise

--- sketches/sketch-04.js
+++ sketches/sketch-04.js
@@ -1,16 +1,19 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: false
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
+  return ({ context, width, height, frame}) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const cols = 10;
-    const rows = 10;
+    const cols = 100;
+    const rows = 100;
     const numCells = cols * rows;
 
     const gridw = width * 0.8;
@@ -29,12 +32,19 @@ const sketch = () => {
       const w = cellw * 0.8;
       const h = cellh * 0.8;
 
+      const n = random.noise2D(x + frame * 10, y + frame * 10, frequency = 0.001);
+      const angle = n * Math.PI * 0.2;
+      //const scale = (n + 1) / 2 * 30;
+      const scale = math.mapRange(n, -1, 1, 1, 50);
+
       context.save();
       context.translate(x, y);
       context.translate(margx, margy);
       context.translate(cellw * 0.5, cellh * 0.5);
 
-      context.lineWidth = 4;
+      context.rotate(angle);
+
+      context.lineWidth = scale;
 
       context.beginPath();
       context.moveTo(w * -0.5, 0);
